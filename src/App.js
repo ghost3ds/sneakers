6f6e59c172ec7946afcb5fd9c78b4d30fd8e500d
Dmitrii Chetyreshnikov <d.chet@bk.ru>
2023-10-28 21:42:23 +0300

    cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import { CgShoppingCart } from 'react-icons/cg';
-import { MdOutlineFavoriteBorder } from 'react-icons/md';
+import { MdOutlineFavoriteBorder, MdRemoveCircle } from 'react-icons/md';
 import { AiOutlineUser, AiOutlinePlusCircle } from 'react-icons/ai';
 import { IconContext } from 'react-icons';
 
 function App() {
   return (
     <div className="wrapper">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+          <div className="cartItems">
+            <div className="cartItem">
+              <img width={70} height={70} src="img/item1.jpg" alt="Nike Blazer" />
+              <div className="cartPrice">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+                <MdRemoveCircle size={21} className="removeBtn" />
+              </IconContext.Provider>
+            </div>
+            <div className="cartItem">
+              <img width={70} height={70} src="img/item2.jpg" alt="Nike Blazer" />
+              <div className="cartPrice">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+                <MdRemoveCircle size={21} className="removeBtn" />
+              </IconContext.Provider>
+            </div>
+          </div>
+          <div className="totalPrice">
+            <span>Итого:</span>
+            <div className="line"></div>
+            <b>21 498 руб.</b>
+          </div>
+          <button className="greenBtn">
+            Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+          </button>
+        </div>
+      </div>
+
       <header>
         <div className="leftSide">
           <img src="img/logo.svg" alt="logo" />
